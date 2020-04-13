e8addd1d0eac54deb67f1af4387458c213331d47
Alistair Evans <alistairjevans@gmail.com>
2020-04-13 09:58:21 +0100

    Using the interface instead to figure out what is going on with extension event handlers.

--- src/AutoStep.Web/WebEventHandler.cs
+++ src/AutoStep.Web/WebEventHandler.cs
@@ -7,9 +7,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AutoStep.Web
 {
-    public class WebEventHandler : BaseEventHandler
+    public class WebEventHandler : IEventHandler
     {
-        public override async ValueTask OnFeature(IServiceProvider scope, FeatureContext ctxt, Func<IServiceProvider, FeatureContext, ValueTask> nextHandler)
+        public ValueTask OnExecute(IServiceProvider scope, RunContext ctxt, Func<IServiceProvider, RunContext, ValueTask> nextHandler)
+        {
+            return nextHandler(scope, ctxt);
+        }
+
+        public async ValueTask OnFeature(IServiceProvider scope, FeatureContext ctxt, Func<IServiceProvider, FeatureContext, ValueTask> nextHandler)
         {
             // Create a browser.
             var browser = scope.GetRequiredService<Browser>();
@@ -17,6 +22,21 @@ namespace AutoStep.Web
             browser.Initialise();
 
             await nextHandler(scope, ctxt);
+        }
+
+        public ValueTask OnScenario(IServiceProvider scope, ScenarioContext ctxt, Func<IServiceProvider, ScenarioContext, ValueTask> nextHandler)
+        {
+            return nextHandler(scope, ctxt);
+        }
+
+        public ValueTask OnStep(IServiceProvider scope, StepContext ctxt, Func<IServiceProvider, StepContext, ValueTask> nextHandler)
+        {
+            return nextHandler(scope, ctxt);
+        }
+
+        public ValueTask OnThread(IServiceProvider scope, ThreadContext ctxt, Func<IServiceProvider, ThreadContext, ValueTask> nextHandler)
+        {
+            return nextHandler(scope, ctxt);
         }
     }
 }
